e119439dbfc86dc93e3176334f8d6c7a9dcad512
Arthur Tchamokouen <rodrigue.arthur@gmail.com>
2019-07-10 22:18:29 +0200

    lot of modif UC1

---
+++ Scala/src/main/scripts/initApplicationStructure.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#app=$1 {jo, ...}
+#env=$2 {dev, uat, prd}
+#technical user id=$3
+
+userid=$3
+
+function create_database
+{
+	#space=$3 {raw, app, lake}
+
+	beeline -u 'jdbc:hive2://localhost:10000/default' -e "CREATE DATABASE IF NOT EXISTS $2_$3_$1 LOCATION '/$2/$3/$1/hive';"
+}
+
+function create_hdfs_structure
+{
+
+	#app=$1 {jo, ...}
+    #env=$2 {dev, uat, prd}
+	#space=$3 {raw, app, lake}
+	#folder_to_create=$4
+    #technical user id=$5
+
+	hdfs dfs -mkdir -p /$2/$3/$1/$4
+
+	hdfs dfs -chown $5 $2/$3/$1/$4
+	hdfs dfs -chmod 773 $2/$3/$1/$4
+}
+
+echo "Application name= $1"
+
+sudo -i
+su - hdfs
+
+#création de l'espace hdfs raw avec affectation des droits au bon groupe
+create_hdfs_structure $1 $2 "raw" "hive" $3
+create_hdfs_structure $1 $2 "raw" "data" $3
+
+#création de l'espace hdfs raw avec affectation des droits au bon groupe
+create_hdfs_structure $1 $2 "lake" "hive" $3
+create_hdfs_structure $1 $2 "lake" "data" $3
+
+#création de l'espace hdfs raw avec affectation des droits au bon groupe
+create_hdfs_structure $1 $2 "app" "hive" $3
+create_hdfs_structure $1 $2 "app" "data" $3
+create_hdfs_structure $1 $2 "app" "bin" $3
+
+#création de la base de données raw
+create_database $1 $2 "raw"
+
+#création de la base de données lake
+create_database $1 $2 "lake"
+
+#création de la base de données app
+create_database $1 $2 "app"
+
+
+echo "Application structure initialized"
+
+exit
+
